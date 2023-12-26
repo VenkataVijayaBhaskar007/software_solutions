26de2d057ef3d4004d011d9270680822220730f0
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-26 22:23:41 +0530

    added changes in testimonials tab errors

--- components/Testimonial/SingleTestimonial.tsx
+++ components/Testimonial/SingleTestimonial.tsx
@@ -1,7 +1,7 @@
 
 import Image from "next/image";
 
-const SingleTestimonial = ({ review }: { review: Testimonial }) => {
+const SingleTestimonial = ({ review  }) => {
   const { name, designation, image, content } = review;
   return (
     <div className="rounded-lg bg-white p-9 pt-7.5 shadow-solid-9 dark:border dark:border-strokedark dark:bg-blacksection dark:shadow-none">
