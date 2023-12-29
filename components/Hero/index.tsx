fd8c35f4945113154a68d80b710c0aee20dd02b4
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-29 10:43:56 +0530

    added a section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -163,7 +163,156 @@ const Hero = () => {
   </motion.div>
 </section>
 
+<section id="works" className="relative bg-gray-900 py-10 sm:py-16 lg:py-24 mb-10">
+<motion.div
+    variants={{
+      hidden: {
+        opacity: 0,
+        y: -40,
+      },
+      visible: {
+        opacity: 1,
+        y: 0,
+      },
+    }}
+    initial="hidden"
+    whileInView="visible"
+    transition={{ duration: 1, delay: 0.5 }}
+  
+    className="animate_top "
+  >
+    <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <motion.div
+    variants={{
+      hidden: {
+        opacity: 0,
+        y: -40,
+      },
+      visible: {
+        opacity: 1,
+        y: 0,
+      },
+    }}
+    initial="hidden"
+    whileInView="visible"
+    transition={{ duration: 1, delay: 0.5 }}
+  
+    className="animate_top "
+  >
+        <div className="max-w-2xl mx-auto text-center">
+            <h2 className="text-4xl text-white font-regular mx-auto md:text-5xl lg:text-4xl">Roadmap of Our Developing Process</h2>
+            <p className="max-w-2xl mx-auto mt-2 text-base text-gray-400 leading-relaxed md:text-2xl">
+                Our AI solution will help you from start to finish
+            </p>
+        </div>
+        </motion.div>
+        <div className="relative mt-12 lg:mt-20">
+        <div className="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28">
+  <img
+    alt=""
+    loading="lazy"
+    width="1000"
+    height="500"
+    decoding="async"
+    data-nimg="1"
+    className="w-full text-transparent"
+    src="https://cdn.rareblocks.xyz/collection/celebration/images/steps/2/curved-dotted-line.svg"
+  />
+</div>
 
+            <div className="relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12">
+            <motion.div
+    variants={{
+      hidden: {
+        opacity: 0,
+        y: -40,
+      },
+      visible: {
+        opacity: 1,
+        y: 0,
+      },
+    }}
+    initial="hidden"
+    whileInView="visible"
+    transition={{ duration: 1, delay: 0.9 }}
+  
+    className="animate_top "
+  >
+                <div>
+                    <div
+                        className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
+                        <span className="text-xl font-semibold text-gray-700">1</span>
+                    </div>
+                    <h3 className="mt-6 text-xl  text-white font-semibold leading-tight md:mt-10">Planning & Designing</h3>
+                    <p className="mt-4 text-base text-gray-400 md:text-lg">
+                        Select template accourding to your requirement
+                    </p>
+                </div>
+                </motion.div>
+                <motion.div
+    variants={{
+      hidden: {
+        opacity: 0,
+        y: -40,
+      },
+      visible: {
+        opacity: 1,
+        y: 0,
+      },
+    }}
+    initial="hidden"
+    whileInView="visible"
+    transition={{ duration: 1, delay: 1.2 }}
+  
+    className="animate_top "
+  >
+                <div>
+                    <div
+                        className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
+                        <span className="text-xl font-semibold text-gray-700">2</span>
+                    </div>
+                    <h3 className="mt-6 text-xl text-white font-semibold leading-tight md:mt-10">Functionality & Testing</h3>
+                    <p className="mt-4 text-base text-gray-400 md:text-lg">
+                        Put in your personalized details and let the AI do the rest.
+                    </p>
+                </div>
+                </motion.div>
+                <motion.div
+    variants={{
+      hidden: {
+        opacity: 0,
+        y: -40,
+      },
+      visible: {
+        opacity: 1,
+        y: 0,
+      },
+    }}
+    initial="hidden"
+    whileInView="visible"
+    transition={{ duration: 1, delay: 1.5 }}
+  
+    className="animate_top "
+  >
+                <div>
+                    <div
+                        className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
+                        <span className="text-xl font-semibold text-gray-700">3</span>
+                    </div>
+                    <h3 className="mt-6 text-xl text-white font-semibold leading-tight md:mt-10">Monitoring for Updates</h3>
+                    <p className="mt-4 text-base text-gray-400 md:text-lg">
+                        Use output as you like
+                    </p>
+                </div>
+                </motion.div>
+            </div>
+        </div>
+    </div>
+    <div className="absolute inset-0 m-auto max-w-xs h-357 blur-118 sm:max-w-md md:max-w-lg bg-radial-gradient"
+    style={{ background: 'radial-gradient(1.89deg, rgba(34, 78, 95, 0.4) -1000%, rgba(191, 227, 205, 0.26) 1500.74%, rgba(34, 140, 165, 0.41) 56.49%, rgba(28, 47, 99, 0.11) 1150.91%)' }}>
+</div>
+</motion.div>
+</section>
 
 
 
