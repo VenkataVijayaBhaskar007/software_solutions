19844baad03ff31282b6cdc5e2e3830f59282bc1
V.VenkataVijayaBhaskar <venkatavijayabhaskarvadada007@gmail.com>
2024-01-10 20:43:04 +0530

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -162,7 +162,7 @@ const Header = () => {
             </Link> */}
 
             <Link
-              href="/support"
+              href="tel:7993611399"
               className="flex items-center justify-center rounded-full bg-primary px-5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-blue-900 hover:translate-y-[-8px]"
             >
               contact US 
